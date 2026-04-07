7d774be01de81be6649f6196e4e0e119d6878bcd
Omri Cohen <88517165+OCDev1@users.noreply.github.com>
2026-04-07 15:05:55 +0300

    Add: pacific-atlantic-water-flow - submission-3

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-3.py
@@ -0,0 +1,39 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        # we run dfs from pacific cells, run dfs from atlantic cells and check the intersection
+        ans = []
+        pacific = set()
+        atlantic = set()
+        rows = len(heights)
+        cols = len(heights[0])
+
+        def dfs(r,c,ocean):
+            # if visited - skip
+            if (r,c) in ocean:
+                return
+            # out of bounds check
+            if not (0 <= r < rows) or not (0 <= c < cols):
+                return
+            
+            ocean.add((r,c))
+            
+            # Recurse on valid neighbors
+            for dr,dc in [(r-1,c),(r+1,c),(r,c-1),(r,c+1)]:
+                if (0 <= dr < rows) and (0 <= dc < cols) and heights[dr][dc] >= heights[r][c]:
+                    dfs(dr,dc,ocean)
+        
+        # run check from cells bordering the ocean
+        for c in range(cols):
+            dfs(0,c,pacific)
+        for r in range(rows):
+            dfs(r,0,pacific)
+        for r in range(rows):
+            dfs(r,cols-1,atlantic)
+        for c in range(cols):
+            dfs(rows-1,c,atlantic)
+        
+        # check for cells accessible from both Pacific and Atlantic
+        for coord in pacific:
+            if coord in atlantic:
+                ans.append(list(coord))
+        return ans

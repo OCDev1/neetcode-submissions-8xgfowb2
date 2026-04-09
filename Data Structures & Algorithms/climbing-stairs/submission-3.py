baaccbf01ace4cccd5d6dbeac4632f98d0d5d45a
Omri Cohen <88517165+OCDev1@users.noreply.github.com>
2026-04-09 17:18:09 +0300

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # f(n) = f(n-1) + f(n-2)
+        # use dp
+        arr = [0] * (n+1)
+
+        arr[0],arr[1] = 1,1
+
+        for i in range(2,n+1):
+            arr[i] = arr[i-1] + arr[i-2]
+
+        return arr[n]

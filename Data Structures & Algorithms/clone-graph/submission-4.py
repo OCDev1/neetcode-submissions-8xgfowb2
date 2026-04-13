677b57b6bb5e136df973ca3ce36c7e19e2823222
Omri Cohen <88517165+OCDev1@users.noreply.github.com>
2026-04-13 11:55:21 +0300

    Update: clone-graph - submission-4

--- Data Structures & Algorithms/clone-graph/submission-4.py
+++ Data Structures & Algorithms/clone-graph/submission-4.py
@@ -8,20 +8,23 @@ class Node:
 
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        # we run dfs on the graph, if a node doesnt have a copy in oldToNew
+        # we create one and add to oldToNew
+        # we must copy the val and the neighbors (they should be copies too)
         oldToNew = {}
 
         def dfs(node):
             if node in oldToNew:
-                return oldToNew[node]
+                return oldToNew[node]   # return the existing copy
+            if not node:
+                return
             
             copy = Node(node.val)
             oldToNew[node] = copy
-
+            # deep copy the neighbors of the node
             for nei in node.neighbors:
                 copy.neighbors.append(dfs(nei))
+            # return the copy
             return copy
-            
-        if node:
-            return dfs(node)
-        else:
-            return
+        
+        return dfs(node)

fa4dafb0a1d0fe9edca425caeb9efe7078f87dfe
Omri Cohen <88517165+OCDev1@users.noreply.github.com>
2026-04-16 17:10:45 +0300

    Add: redundant-connection - submission-7

---
+++ Data Structures & Algorithms/redundant-connection/submission-7.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        # We need to detect a cycle and remove an edge in the cycle
+        # if we detect it in node x we remove an edge that touches node x
+        # build adjacency list
+        adjList = defaultdict(list)
+        seen = set()
+        
+        def dfs(node: int,parent: int):
+            if node in seen:
+                return True
+            
+            seen.add(node)
+
+            for nei in adjList[node]:
+                if nei == parent:
+                    continue
+                if dfs(nei,node):
+                    return True
+            return False
+
+        # every iteration we add an edge and run the check
+        # if true - the last edge caused the cycle
+        # undirected - edges go both ways
+        for a,b in edges:
+            adjList[a].append(b)
+            adjList[b].append(a)
+            seen = set()
+            if dfs(a,-1):
+                return [a,b]
